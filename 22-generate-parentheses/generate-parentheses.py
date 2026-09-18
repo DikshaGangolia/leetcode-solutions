3fd85212da403db754fa9c988148d6b21b2cce10
Diksha.. <dikshagangolia@gmail.com>
2026-09-18 18:28:49 +0530

    Time: 1 ms (60.04%) | Memory: 12.6 MB (87.91%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,24 @@
+class Solution:
+    def generateParenthesis(self, n):
+        result = []
+        def backtrack(current, open_count, close_count):
+            # We have used all brackets
+            if len(current) == 2 * n:
+                result.append(current)
+                return
+            # Add opening bracket
+            if open_count < n:
+                backtrack(
+                    current + "(",
+                    open_count + 1,
+                    close_count
+                )
+            # Add closing bracket
+            if close_count < open_count:
+                backtrack(
+                    current + ")",
+                    open_count,
+                    close_count + 1
+                )
+        backtrack("", 0, 0)
+        return result
